5421cd71b7eba5b894a5689bbd1453ad237379e9
LinHeLurking <linhe.lurking@gmail.com>
2021-08-07 03:52:40 +0800

    Phase 1 search interfaces.

--- two_phase_solver.h
+++ two_phase_solver.h
@@ -1,6 +1,7 @@
 #ifndef CUBE_SOLVER_TWO_PHASE_SOLVER_H
 #define CUBE_SOLVER_TWO_PHASE_SOLVER_H
 
+#include <cstdint>
 #include <vector>
 
 #include "cube_def.h"
@@ -8,7 +9,24 @@
 
 namespace Cube {
     class TwoPhaseSolver {
+    public:
+        TwoPhaseSolver() = default;
+
+        std::vector<BasicMoveName> phaseOneSearch(const CubeStatus &start) {
+
+        }
+
+        std::vector<BasicMoveName> phaseTwoSearch(const CubeStatus &start) {
+
+        }
+
     protected:
+        static constexpr UDSliceCnt = 495;
+
+        uint32_t UDSliceHeuristic_[UDSliceCnt];
+        uint32_t UDSliceMoveTable[UDSliceCnt];
+
+
         ConstantFactory &constantFactory = ConstantFactory.getInstance();
         MoveFactory &moveFactory = MoveFactory.getInstance();
 
@@ -16,16 +34,7 @@ namespace Cube {
 
         }
 
-        void buildIDAStarLowerBound() {
-
-        }
-
-    public:
-        std::vector<BasicMoveName> phaseOneSearch(const CubeStatus &start) {
-
-        }
-
-        std::vector<BasicMoveName> phaseTwoSearch(const CubeStatus &start) {
+        void buildUDSliceHeuristic() {
 
         }
     };
